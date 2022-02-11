144718dfee59450aedafd149a382495a63034abc
Michael Enslein <michael_enslein@Michaels-MacBook-Pro.local>
2022-02-11 11:52:21 -0500

    received all projects

--- api/projects/projects-router.js
+++ api/projects/projects-router.js
@@ -1,1 +1,23 @@
 // Write your "projects" router here!
+const express = require('express');
+
+const Projects = require('./projects-model')
+
+const router = express.Router();
+
+router.get('/', (req, res, next) => {
+    Projects.get()
+    .then(projects => {
+        res.json(projects)
+    })
+    .catch(next)
+})
+
+router.use((err, req, res, next) => {
+    res.status(err.status || 500).json({
+        message: err.message,
+        stack: err.stack
+    })
+})
+
+module.exports = router
